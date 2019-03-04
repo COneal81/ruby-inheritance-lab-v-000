1dcdbac277cfc5e4698977d6c3bb399d11fee076
Kari O'Neal <triadfirewood@gmail.com>
2019-03-04 13:23:19 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -4,9 +4,6 @@ class Teacher < User
   
   KNOWLEDGWE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
   
-  def initialize
-    KNOWLEDGWE << self
-  end
   
   def teach 
    KNOWLEDGWE.sample
